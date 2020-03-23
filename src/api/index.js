cdca05234d6d5d579b9b43a6dc0be50755c5aa1f
difo23 <diffozanzan@gmail.com>
2020-03-23 00:45:34 -0400

    Modificacion react

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
 	//baseURL: `http://192.168.0.22:8626/`
-	baseURL: `http://localhost:8626/`
+	baseURL: `https://cemasapi.herokuapp.com/`
 });
