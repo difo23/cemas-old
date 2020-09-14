905e61ee355654d491ee9cc825e2486fb500f333
difo23 <diffozanzan@gmail.com>
2020-09-14 17:05:06 -0400

    URL

--- src/api/url.js
+++ src/api/url.js
@@ -1,5 +1,12 @@
 
-const URL = process.env.REACT_APP_API_URL;
+let URL
+
+if(process.env.REACT_APP_ENV === 'dev'){
+    URL = process.env.REACT_APP_API_URL;
+}else {
+
+    URL = process.env.API_URL;
+}
 
 
 export default URL;
